68ac7afc2ec6e7cb3fc337cd5b2f07db13c5f6f7
Rosie Schofield <rschofield00@gmail.com>
2023-07-10 15:45:54 +0100

    made functional dog app that accumulates dog images

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,44 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+interface dogImage {
+  message: string;
+  status: string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [dogImage, setdogImage] = useState<dogImage["message"][]>([]);
 
-  const handleGetJoke = async () => {
+  const handleGetDogImage = async () => {
     const response = await fetch(
-      "https://jokestemp.neillbogie.repl.co/jokes/general/random"
+      "https://dog.ceo/api/breeds/image/random"
     );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
+    const jsonBody: dogImage = await response.json();
+    setdogImage(prevImages=>[...prevImages,jsonBody["message"]]);
   };
 
-  // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
-  // };
+   /*const handleGetDogImage = () => {
+     fetch("https://dog.ceo/api/breeds/image/random")
+       .then((response) => response.json())
+       .then((jsonBody: dogImage) => setdogImage(jsonBody));
+   };*/
 
-  if (joke) {
+  if (dogImage.length>0) {
     return (
       <div>
-        <h1>Joke app</h1>
-        <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
-        </details>
-        <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <h1>Dog Image app</h1>
+        {dogImage.map((x,index)=><img src = {x} alt="my dog" ></img>)}
+        <button onClick={handleGetDogImage}>Get another dog image</button>
       </div>
     );
   } else {
     return (
       <div>
-        <h1>Joke app</h1>
+        <h1>Dog Image app</h1>
         <p>
           Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
+          dog image from an API!
         </p>
-        <button onClick={handleGetJoke}>Get joke</button>
+        <button onClick={handleGetDogImage}>Get dog image</button>
       </div>
     );
   }
